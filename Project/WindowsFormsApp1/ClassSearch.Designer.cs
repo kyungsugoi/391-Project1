d9a3fe02f683141d69ad7535c198e4618ff48feb
BeastlyOrca <118201727+BeastlyOrca@users.noreply.github.com>
2024-01-24 17:19:00 -0700

    basic ui

--- Project/WindowsFormsApp1/ClassSearch.Designer.cs
+++ Project/WindowsFormsApp1/ClassSearch.Designer.cs
@@ -31,11 +31,12 @@
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.lstClassBox = new System.Windows.Forms.ListBox();
             this.cmbSemester = new System.Windows.Forms.ComboBox();
+            this.btnShop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(62, 78);
+            this.txtSearch.Location = new System.Drawing.Point(62, 65);
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(680, 20);
             this.txtSearch.TabIndex = 0;
@@ -44,7 +45,7 @@
             // lstClassBox
             // 
             this.lstClassBox.FormattingEnabled = true;
-            this.lstClassBox.Location = new System.Drawing.Point(62, 124);
+            this.lstClassBox.Location = new System.Drawing.Point(62, 91);
             this.lstClassBox.Name = "lstClassBox";
             this.lstClassBox.Size = new System.Drawing.Size(680, 277);
             this.lstClassBox.TabIndex = 1;
@@ -58,16 +59,27 @@
             this.cmbSemester.TabIndex = 2;
             this.cmbSemester.Text = "Semester";
             // 
+            // btnShop
+            // 
+            this.btnShop.Location = new System.Drawing.Point(626, 374);
+            this.btnShop.Name = "btnShop";
+            this.btnShop.Size = new System.Drawing.Size(116, 42);
+            this.btnShop.TabIndex = 3;
+            this.btnShop.TabStop = false;
+            this.btnShop.Text = "Shopping Cart";
+            this.btnShop.UseVisualStyleBackColor = true;
+            // 
             // ClassSearch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 428);
+            this.Controls.Add(this.btnShop);
             this.Controls.Add(this.cmbSemester);
             this.Controls.Add(this.lstClassBox);
             this.Controls.Add(this.txtSearch);
             this.Name = "ClassSearch";
-            this.Text = "Form2";
+            this.Text = "Classes";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,5 +90,6 @@
         private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.ListBox lstClassBox;
         private System.Windows.Forms.ComboBox cmbSemester;
+        private System.Windows.Forms.Button btnShop;
     }
 }
--- Project/WindowsFormsApp1/Enroll.Designer.cs
+++ Project/WindowsFormsApp1/Enroll.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbSemester = new System.Windows.Forms.ComboBox();
+            this.lstClassBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmbSemester
+            // 
+            this.cmbSemester.FormattingEnabled = true;
+            this.cmbSemester.Location = new System.Drawing.Point(57, 25);
+            this.cmbSemester.Name = "cmbSemester";
+            this.cmbSemester.Size = new System.Drawing.Size(121, 21);
+            this.cmbSemester.TabIndex = 8;
+            this.cmbSemester.Text = "Semester";
+            // 
+            // lstClassBox
+            // 
+            this.lstClassBox.FormattingEnabled = true;
+            this.lstClassBox.Location = new System.Drawing.Point(57, 63);
+            this.lstClassBox.Name = "lstClassBox";
+            this.lstClassBox.Size = new System.Drawing.Size(680, 277);
+            this.lstClassBox.TabIndex = 7;
+            // 
+            // Enroll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 368);
+            this.Controls.Add(this.cmbSemester);
+            this.Controls.Add(this.lstClassBox);
+            this.Name = "Enroll";
             this.Text = "Enroll";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbSemester;
+        private System.Windows.Forms.ListBox lstClassBox;
     }
 }
