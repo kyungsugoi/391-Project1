2a069f9412a582117e898fac1f11de02f3b9a22a
BeastlyOrca <118201727+BeastlyOrca@users.noreply.github.com>
2024-02-08 18:24:09 -0700

    database connect

--- Project/WindowsFormsApp1/Login.cs
+++ Project/WindowsFormsApp1/Login.cs
@@ -2,40 +2,85 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace WindowsFormsApp1
 {
     public partial class Login : Form
     {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
         public Login()
         {
             InitializeComponent();
+
+            // Nadir's Sql:            ADD YOUR CONNECTION STRING AND COMMENT OUT MINE OR JUST CHANGE SERVER NAME TO YOURS
+            String connectionString = "Server = DESKTOP-5HTNF3D\\SQLEXPRESS; Database = CMPT391_1; Trusted_Connection = yes;";
+            SqlConnection myConnection = new SqlConnection(connectionString);
+
+            try
+            {
+                myConnection.Open();
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+
+            }
         }
 
+      
 
         //  ------------------------ BUTTON CLICK ----------------------------------------
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            string user = txtUsername.Text;
-            string pass = txtPassword.Text;
-            string u = "user1";
-            string p = "pass1";
-            
-            if (String.Equals(user, u) && String.Equals(pass, p))
-            {
-                ClassSearch classSearch = new ClassSearch();
-                classSearch.Show();
-                this.Hide();
+            int UserCheck = 0;
+            myCommand.CommandText = "select * from Student";
 
-            } else
+            try
             {
-                lblError.Visible = true; 
+                myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    if (myReader["firstName"].ToString().Trim() == txtUsername.Text)
+                    {
+                        if (myReader["password"].ToString().Trim() == txtPassword.Text)
+                        {
+                            UserCheck = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (UserCheck == 1)
+                {
+                    ClassSearch classSearch = new ClassSearch();
+                    classSearch.Show();
+                    this.Hide();
+
+                }
+
+                else MessageBox.Show("Username or Password Invalid");
+
+                myReader.Close();
             }
+            catch (Exception e3)
+            {
+                MessageBox.Show(e3.ToString(), "Error");
+            }
+
         }
     }
 }
