22301695c66fc6a0d53c7fb772a9065ad88b4541
Jesse Emery <emeryj6@mymacewan.ca>
2024-02-12 09:11:10 -0700

    Removed Identity from tables, Insert script more verbose, courseID now char(7), departmentHeadID updates properly

--- SQL Scripts/391_INSERT.sql
+++ SQL Scripts/391_INSERT.sql
@@ -1,49 +1,61 @@
 USE [CMPT391_1]
 GO
 
-SET IDENTITY_INSERT [dbo].[Department] OFF
-GO
-INSERT INTO [dbo].[Department] (departmentName)
+INSERT INTO [dbo].[Department] (departmentID, departmentName)
 VALUES
-('Computer Science'),
-('Mathematics'),
-('Physics'),
-('Biology'),
-('Chemistry'),
-('Statistics'),
-('Earth Science');
+(1, 'Computer Science'),
+(2, 'Mathematics'),
+(3, 'Physics'),
+(4, 'Biology'),
+(5, 'Chemistry'),
+(6, 'Statistics'),
+(7, 'Earth Science');
 GO
 
-SET IDENTITY_INSERT [dbo].[Instructor] OFF
-GO
-INSERT INTO [dbo].[Instructor] (firstName, lastName, departmentID)
+INSERT INTO [dbo].[Instructor] (instructorID, firstName, lastName, departmentID)
 VALUES
-('Bailie', 'Dubs', 1),
-('Rubina', 'Boxill', 1),
-('Roi', 'Hundy', 1),
-('Larissa', 'Savege', 2),
-('Llewellyn', 'Vardy', 2),
-('Thomasina', 'Bateup', 2),
-('Godfrey', 'Hamner', 3),
-('Corina', 'Shillan', 3),
-('Annetta', 'Nyssens', 3),
-('La verne', 'Skinley', 4),
-('Ranna', 'Huscroft', 4),
-('Bekki', 'Oaks', 4);
+(1, 'Bailie', 'Dubs', 1),
+(2, 'Rubina', 'Boxill', 1),
+(3, 'Roi', 'Hundy', 1),
+(4, 'Larissa', 'Savege', 2),
+(5, 'Llewellyn', 'Vardy', 2),
+(6, 'Thomasina', 'Bateup', 2),
+(7, 'Godfrey', 'Hamner', 3),
+(8, 'Corina', 'Shillan', 3),
+(9, 'Annetta', 'Nyssens', 4),
+(10, 'La verne', 'Skinley', 5),
+(11, 'Ranna', 'Huscroft', 6),
+(12, 'Bekki', 'Oaks', 7);
 GO
 
-/*
-TODO add departmentHead, use ALTER?
-INSERT INTO [dbo].[Department] (departmentHeadID)
-VALUES
-('Computer Science'),
-('Mathematics'),
-('Physics'),
-('Biology');
-GO*/
+UPDATE Department
+SET departmentHeadID = 1
+WHERE departmentID = 1;
 
-SET IDENTITY_INSERT [dbo].[Student] ON
-GO
+UPDATE Department
+SET departmentHeadID = 4
+WHERE departmentID = 2;
+
+UPDATE Department
+SET departmentHeadID = 7
+WHERE departmentID = 3;
+
+UPDATE Department
+SET departmentHeadID = 9
+WHERE departmentID = 4;
+
+UPDATE Department
+SET departmentHeadID = 10
+WHERE departmentID = 5;
+
+UPDATE Department
+SET departmentHeadID = 11
+WHERE departmentID = 6;
+
+UPDATE Department
+SET departmentHeadID = 12
+WHERE departmentID = 7;
+
 INSERT INTO [dbo].[Student] (studentID, firstName, lastName, activeStudent, password)
 VALUES
 (1, 'Malinda', 'Aingel', 0, '12345678'),
@@ -57,84 +69,72 @@ VALUES
 (9, 'Hugh', 'Drillot', 1, 'dA2.9uf7y}'),
 (10, 'Francene', 'Scrivens', 1, 'lD9(ITxwN+(');
 GO
-SET IDENTITY_INSERT [dbo].[Student] OFF
-GO
 
-SET IDENTITY_INSERT [dbo].[Course] OFF
-GO
-INSERT INTO [dbo].[Course] (courseName, courseDescription, credits, departmentID)
+INSERT INTO [dbo].[Course] (courseID, courseName, courseDescription, credits, departmentID)
 VALUES
-('CMPT 101', 'Introductory computer science', 3, 1),
-('CMPT 201', 'Intermediate computer science topics', 3, 1),
-('CMPT 301', 'Advanced computer science topics', 3, 1),
-('MATH 101', 'Introductory mathematics', 3, 2),
-('MATH 201', 'Intermediate mathematics topics', 3, 2),
-('MATH 301', 'Advanced mathematics topics', 3, 2),
-('PHYS 101', 'Introductory physics', 3, 3),
-('PHYS 201', 'Intermediate physics topics', 3, 3),
-('PHYS 301', 'Advanced physics topics', 3, 3),
-('BIOL 101', 'Introductory biology', 3, 4),
-('BIOL 201', 'Intermediate biology topics', 3, 4),
-('BIOL 301', 'Advanced biology topics', 3, 4);
+('CMPT101', 'Introductory computer science', 'This course provides a breadth-first introductory treatment of concepts in computing science for students with little or no programming background.', 3, 1),
+('CMPT201', 'Intermediate computer science topics', 'This course provides an introduction to the principles, methods, tools, and practices of the professional programmer.', 3, 1),
+('CMPT301', 'Advanced computer science topics', 'In this course, students study the object-oriented programming (OOP) paradigm.', 3, 1),
+('MATH101', 'Introductory mathematics', 'This course examines the fundamental concept of limits, differentiation and integration.', 3, 2),
+('MATH201', 'Intermediate mathematics topics', 'This course completes the study of single-variable calculus and introduces students to the basic concepts of multi-variable calculus.', 3, 2),
+('MATH301', 'Advanced mathematics topics', 'This course presents a rigorous treatment of limit processes in one variable.', 3, 2),
+('PHYS101', 'Introductory physics', 'During the course students develop a conceptual understanding of physical principles, develop reasoning and problem-solving skills, and relate these physical principles to real-world situations relevant to biology and medicine.', 3, 3),
+('PHYS201', 'Intermediate physics topics', 'If you’re curious about what Einstein’s famous equation E=mc^2 actually means, or how you could end up twenty years older than your twin, then this is the course for you!', 3, 3),
+('PHYS301', 'Advanced physics topics', 'This course is a study of the fundamental nuclear properties, the shell model, the collective model, stability of nuclei, isotopes, radioactive decay, nuclear reactions, kinematics, conservation laws, nuclear fission and fusion, nuclear reactors, particle accelerators, detectors, a brief introduction to particle physics and the Standard Model.', 3, 3),
+('BIOL101', 'Introductory biology', 'Enter the tiny and wonderful world of cells to gain a deeper understanding of how organisms work.', 3, 4),
+('BIOL201', 'Intermediate biology topics', 'This course studies eukaryotic cellular processes including protein targeting and transport, secretion and endocytosis, and cellular signaling pathways.', 3, 4),
+('BIOL301', 'Advanced biology topics', 'Students apply cell biology techniques to analyze eukaryotic cell function.', 3, 4);
 GO
 
-INSERT INTO [dbo].[Prereq]
+INSERT INTO [dbo].[Prereq] (courseID, prereqID)
 VALUES
-(2, 1),
-(2, 4),
-(3, 1),
-(5, 4),
-(6, 5),
-(8, 7),
-(9, 8),
-(11, 10),
-(12, 11);
+('CMPT201', 'CMPT101'),
+('CMPT201', 'MATH101'),
+('CMPT301', 'CMPT201'),
+('MATH201', 'MATH101'),
+('MATH301', 'MATH201'),
+('PHYS201', 'PHYS101'),
+('PHYS301', 'PHYS201'),
+('BIOL201', 'BIOL101'),
+('BIOL301', 'BIOL201');
 GO
 
-SET IDENTITY_INSERT [dbo].[TimeSlot] OFF
-GO
-INSERT INTO [dbo].[TimeSlot] (day, startTime, endTime)
+INSERT INTO [dbo].[TimeSlot] (timeSlotID, day, startTime, endTime)
 VALUES
-('Monday', '10:00', '11:00'),
-('Tuesday', '10:00', '11:00'),
-('Wednesday', '10:00', '11:00'),
-('Thursday', '10:00', '11:00'),
-('Friday', '10:00', '11:00');
+(1, 'Monday', '10:00', '11:00'),
+(2, 'Tuesday', '10:00', '11:00'),
+(3, 'Wednesday', '10:00', '11:00'),
+(4, 'Thursday', '10:00', '11:00'),
+(5, 'Friday', '10:00', '11:00');
 GO
 
-SET IDENTITY_INSERT [dbo].[Classroom] OFF
-GO
-INSERT INTO [dbo].[Classroom] (classroomName, capacity)
+INSERT INTO [dbo].[Classroom] (classroomID, classroomName, capacity)
 VALUES
-('5-212', 40),
-('5-140', 20),
-('6-110', 30),
-('7-230', 50),
-('11-232', 40);
+(1, '5-212', 40),
+(2, '5-140', 20),
+(3, '6-110', 30),
+(4, '7-230', 50),
+(5, '11-232', 40);
 GO
 
-SET IDENTITY_INSERT [dbo].[Section] ON
-GO
 INSERT INTO [dbo].[Section] (sectionID, sectionName, sectionType, semester, year, sectionSize, enrolled, instructorID, courseID, classroomID)
 VALUES
-(1, 'AS01', 'Lecture', 'Fall', 2024, 30, 29, 1, 1, 1),
-(2, 'AS02', 'Lecture', 'Fall', 2024, 30, 30, 2, 1, 2),
-(3, 'AS01', 'Lecture', 'Fall', 2024, 30, 20, 1, 2, 4),
-(4, 'AS02', 'Lecture', 'Fall', 2024, 30, 28, 3, 2, 1),
-(5, 'L01', 'Lab', 'Fall', 2024, 30, 29, 2, 2, 3),
-(6, 'L02', 'Lab', 'Fall', 2024, 30, 29, 3, 2, 3),
-(7, 'AS01', 'Lecture', 'Fall', 2024, 30, 29, 2, 3, 3),
-(8, 'S01', 'Seminar', 'Fall', 2024, 30, 29, 3, 3, 3),
-(9, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 1, 1, 3),
-(10, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 2, 2, 3),
-(11, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 3, 3, 3),
-(12, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 4, 4, 3),
-(13, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 5, 5, 3);
-GO
-SET IDENTITY_INSERT [dbo].[Section] OFF
+(1, 'AS01', 'Lecture', 'Fall', 2024, 30, 29, 1, 'CMPT101', 1),
+(2, 'AS02', 'Lecture', 'Fall', 2024, 30, 30, 2, 'CMPT101', 2),
+(3, 'AS01', 'Lecture', 'Fall', 2024, 30, 20, 1, 'CMPT201', 4),
+(4, 'AS02', 'Lecture', 'Fall', 2024, 30, 28, 3, 'CMPT201', 1),
+(5, 'L01', 'Lab', 'Fall', 2024, 30, 29, 2, 'CMPT201', 3),
+(6, 'L02', 'Lab', 'Fall', 2024, 30, 29, 3, 'CMPT201', 3),
+(7, 'AS01', 'Lecture', 'Fall', 2024, 30, 29, 2, 'CMPT301', 3),
+(8, 'S01', 'Seminar', 'Fall', 2024, 30, 29, 3, 'CMPT301', 3),
+(9, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 1, 'CMPT101', 3),
+(10, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 2, 'CMPT201', 3),
+(11, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 3, 'CMPT301', 3),
+(12, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 4, 'MATH101', 3),
+(13, 'AS01', 'Lecture', 'Fall', 2023, 30, 29, 5, 'MATH201', 3);
 GO
 
-INSERT INTO [dbo].[SectionTimeSlot]
+INSERT INTO [dbo].[SectionTimeSlot] (sectionID, timeSlotID)
 VALUES
 (1, 1),
 (1, 3),
@@ -149,7 +149,7 @@ VALUES
 (8, 1);
 GO
 
-INSERT INTO [dbo].[Takes]
+INSERT INTO [dbo].[Takes] (studentID, sectionID, grade)
 VALUES
 (1, 9, 3.0),
 (1, 12, 3.3),
